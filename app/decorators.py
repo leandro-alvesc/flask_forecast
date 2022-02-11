49a144b7f1e7ac173aa206a6f24f17d9c7550bd0
Leandro Alves <leandrofelipecunha@hotmail.com>
2022-02-11 15:25:06 -0300

    feat: created decorator required_id

--- app/decorators.py
+++ app/decorators.py
@@ -1,3 +1,22 @@
+from functools import wraps
+
+from flask import request
+
 
 class Decorators:
-    pass
+    @staticmethod
+    def required_id(f):
+        @wraps(f)
+        def _required_id(*args, **kwargs):
+            body = request.json
+            params = request.args
+
+            if not isinstance(body, dict) and not len(params):
+                return {'code': 'REQUIRED_DATA'}, 400
+
+            data = body or params
+
+            if 'id' not in data:
+                return {'code': 'REQUIRED_ID'}, 400
+            return f(data=data, *args, **kwargs)
+        return _required_id
